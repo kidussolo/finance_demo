e9368b7dfa76b35f41c9d7dcdd63b453ad76a11b
kidus <kidussolomon2004@gmail.com>
2020-10-01 14:29:25 +0300

    add description field to journals

--- app/models.py
+++ app/models.py
@@ -147,6 +147,7 @@ class Vendor(models.Model):
 
 class Journal(models.Model):
     name = models.CharField(max_length=50)
+    description = models.CharField(max_length=255)
     state = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
